1d43dbe1e3901c9ad5daa49e8dd6c7f3518cadcd
Bryon Verdone <bryon@Bryons-MacBook-Pro.local>
2022-07-16 22:22:20 -0400

    added content to hero section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import './index.css';
-
+import Headshot from '../../assets/headshot/lindsey-headshot.jpeg';
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <section className='hero-section'>
+        <div className='hero-content'>
+          <div className='hero-text'>
+            <h2>Lindsey</h2>
+            <span>Freelance Photographer</span>
+            <div className='hero-icons'></div>
+          </div>
+          <div className='hero-img-container'>
+            <img className='hero-img' src={Headshot} alt='' />
+          </div>
+        </div>
+      </section>
+      <section className='hero-section'>
+        <div className='hero-content'>
+          <div className='hero-text'>
+            <h2>Lindsey</h2>
+          </div>
+          <div className='hero-img-container'>
+            <img className='hero-img' src={Headshot} alt='' />
+          </div>
+        </div>
+      </section>
+      <section className='hero-section'>
+        <div className='hero-content'>
+          <div className='hero-text'>
+            <h2>Lindsey</h2>
+          </div>
+          <div className='hero-img-container'>
+            <img className='hero-img' src={Headshot} alt='' />
+          </div>
+        </div>
+      </section>
+      <section className='hero-section'>
+        <div className='hero-content'>
+          <div className='hero-text'>
+            <h2>Lindsey</h2>
+          </div>
+          <div className='hero-img-container'>
+            <img className='hero-img' src={Headshot} alt='' />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
